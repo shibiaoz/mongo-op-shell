f9a8856d4407a336e75f9fd3cb12bda2cc4270d2
shibiaoz <shibiaoz@sina.cn>
2015-10-15 20:19:39 +0800

    export database script

---
+++ export-mongo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mongodump -h 127.0.0.1 -d h5gen -o ~/workplace/back_mongo
+#export all database
+#mongodump -h 127.0.0.1 -o ~/workplace/back_mongo
+#mongodump -h 127.0.0.1 -u username -p password-o ~/workplace/back_mongo
