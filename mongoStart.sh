440e81f7da15d6a8a05e4a09a76c721db485dc3a
shibiaoz <shibiaoz@sina.cn>
2015-10-19 15:29:22 +0800

    --fork

--- mongoStart.sh
+++ mongoStart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# mongod --fork --dbpath=data --logpath=mongolog/mongo.log
+# 部署环境中用--fork以守护进程的方式运行
 mongod --dbpath=data --logpath=mongolog/mongo.log
 echo 'mongo server strarting.....'
 
